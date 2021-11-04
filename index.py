b106aa7c9e55548db9f16e14463669f40fea734a
BenitzCoding <benitzcoding@yahoo.com>
2021-11-04 18:46:02 +0800

    Did stuff...

--- index.py
+++ index.py
@@ -14,14 +14,6 @@ def send_message(token, content):
 	if response.status_code == 200:
 		print(f"<{response.json['username']}> {content}\n")
 
-def create_session(username):
-	payload = {
-		"username": username
-	}
-	response = requests.post(API + "session/create", payload=payload)
-	if response.status_code == 200:
-		print(f"Your Session has been created, here is your token.\n\nTOKEN: {response.json['token']}.")
-
 def scan_for_content(token, username):
 	payload = {
 		"token": token,
@@ -30,6 +22,17 @@ def scan_for_content(token, username):
 	response = requests.post(API + "message-updates", payload)
 	if response.status_code == 404:
 		return None
+	else:
+		message = requests.post(API + "decrypt", response.json())
+		print(f"<{response['username']}> {message.json()}")
+
+def create_session(username):
+	payload = {
+		"username": username
+	}
+	response = requests.post(API + "session/create", payload=payload)
+	if response.status_code == 200:
+		print(f"Your Session has been created, here is your token.\n\nTOKEN: {response.json['token']}.")
 
 def join_session(token, username):
 	payload = {
